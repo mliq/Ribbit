3123754862d1dddf890c92897300c99e36cb57b6
mliqu <l12021l@gmail.com>
2014-10-06 16:20:53 -0500

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Michael Liquori on 10/6/14.
+//  Copyright (c) 2014 Michael Liquori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Michael Liquori on 10/6/14.
+//  Copyright (c) 2014 Michael Liquori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
