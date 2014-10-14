0ed127a4119e0069cab3907a7f0215b796ebc1b3
mliqu <l12021l@gmail.com>
2014-10-14 16:37:54 -0500

    Edit Friends shows all users.

---
+++ EditFriendsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  EditFriendsTableViewController.h
+//  Ribbit
+//
+//  Created by Michael Liquori on 10/14/14.
+//  Copyright (c) 2014 Michael Liquori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsTableViewController : UITableViewController
+
+@property (nonatomic,strong) NSArray *allUsers;
+
+@end
---
+++ Ribbit/EditFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditFriendsViewController.h
+//  Ribbit
+//
+//  Created by Michael Liquori on 10/14/14.
+//  Copyright (c) 2014 Michael Liquori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsViewController : UIViewController
+
+@end
