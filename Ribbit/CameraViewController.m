4f75230479bf6410f7ad86a7034f1520692cbf02
mliqu <l12021l@gmail.com>
2014-10-23 11:13:10 -0500

    Image upload to Parse working.

--- Ribbit/CameraViewController.m
+++ Ribbit/CameraViewController.m
@@ -23,34 +23,8 @@
     self.friendsRelation = [[PFUser currentUser] objectForKey:@"friendsRelation"];
     
     self.recipients = [[NSMutableArray alloc] init];
-    
-    self.imagePicker = [[UIImagePickerController alloc] init];
-    self.imagePicker.delegate = self;// Explicitly tells CameraViewController that this image picker is its delegate
-    
-    //set no allows editing, can cause memory issues, and max Video Duration
-    self.imagePicker.allowsEditing = NO;
-    self.imagePicker.videoMaximumDuration = 10;
-    
-    //Need to set if for devices that do not have camera
-    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        //set source type to camera. Other options photoLibrary, photosAlbum.
-        self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
-    }
-    else {
-        
-    }
-    //Media types
-    self.imagePicker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:self.imagePicker.sourceType];
-    
-    //Method to present modally. Don't want the animation which would show table view first and confuse user. Completion is an optional block to run after imagePicker display is complete.
-    [self presentViewController:self.imagePicker animated:NO completion:nil];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
+
+   }
 
 - (void)viewWillAppear:(BOOL)animated {
     
@@ -72,7 +46,37 @@
             [self.tableView reloadData]; //refresh data
         }
     }];
- 
+    //Only load image picker if no image saved already
+    if (self.image==nil && self.videoFilePath.length==0){
+    
+        self.imagePicker = [[UIImagePickerController alloc] init];
+        self.imagePicker.delegate = self;// Explicitly tells imagePicker that it is delegate of CameraViewController?
+        
+        //set no allows editing, can cause memory issues, and max Video Duration
+        self.imagePicker.allowsEditing = NO;
+        self.imagePicker.videoMaximumDuration = 10;
+        
+        //Need to set if for devices that do not have camera
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            //set source type to camera. Other options photoLibrary, photosAlbum.
+            self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        }
+        else {
+            
+        }
+        //Media types
+        self.imagePicker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:self.imagePicker.sourceType];
+        
+        //Method to present modally. Don't want the animation which would show table view first and confuse user. Completion is an optional block to run after imagePicker display is complete.
+        
+        [self presentViewController:self.imagePicker animated:NO completion:nil];
+        
+        // Uncomment the following line to preserve selection between presentations.
+        // self.clearsSelectionOnViewWillAppear = NO;
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    }
  
 }
 #pragma mark - Table view delegate
@@ -183,14 +187,88 @@
      //Dismiss ImagePickerViewController
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
 - (IBAction)cancel:(id)sender {
-    self.image = nil;
-    self.videoFilePath = nil;
-    [self.recipients removeAllObjects];
-    
+    [self reset];
     [self.tabBarController setSelectedIndex:0];
 }
 
 - (IBAction)send:(id)sender {
+
+    //Defensive programming - check before sending to prevent errors
+
+    if(self.image == nil && [self.videoFilePath length]==0) { //No file
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Try Again!" message:@"Please capture or select a photo or video to share!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+        [self presentViewController:self.imagePicker animated:NO completion:nil];
+    }
+    else { //We do have a file
+        [self uploadMessage];
+        [self reset];
+        [self.tabBarController setSelectedIndex:0];
+    }
 }
+
+#pragma mark - Helper methods
+
+- (UIImage *)resizeImage:(UIImage *)image toWidth:(float)width andHeight:(float)height {
+    CGSize newSize = CGSizeMake(width,height);
+    CGRect newRectangle = CGRectMake(0,0,width,height);
+    UIGraphicsBeginImageContext(newSize);
+        [self.image drawInRect:newRectangle];
+        UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resizedImage;
+}
+
+- (void)reset {
+    self.image = nil;
+    self.videoFilePath = nil;
+    [self.recipients removeAllObjects];
+}
+
+- (void)uploadMessage {
+    NSData *fileData;
+    NSString *fileName;
+    NSString *fileType;
+    
+    if (self.image !=nil) {     // if image, shrink it
+        UIImage *newImage = [self resizeImage:self.image toWidth:320.0f andHeight:480.0f];
+        fileData = UIImagePNGRepresentation(newImage);
+        fileName = @"image.png";
+        fileType = @"image";
+    }
+    else { //VIDEO object
+        fileData = [NSData dataWithContentsOfFile:self.videoFilePath];
+        fileName = @"video.mov";
+        fileType = @"video";
+    }
+    
+    PFFile *file = [PFFile fileWithName:fileName data:fileData contentType:fileType];
+    [file saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if(error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"An error occurred!" message:@"Please try sending your image/video again!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+        else {     // upload the file & message details
+            PFObject *message = [PFObject objectWithClassName:@"Messages"];
+            [message setObject:file forKey:@"file"];
+            [message setObject:fileType forKey:@"fileType"];
+            [message setObject:self.recipients forKey:@"recipientIds"];
+            [message setObject:[[PFUser currentUser] objectId] forKey:@"senderId"];
+            [message setObject:[[PFUser currentUser] username] forKey:@"senderName"];
+            [message saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if(error) {
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"An error occurred!" message:@"Please try sending your image/video again!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                }
+                else {
+                    
+                }
+            }];
+        }
+    }];
+}
+
 @end
